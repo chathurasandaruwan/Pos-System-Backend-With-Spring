e73eaff6c7e786696aacdd39134b35ba16a80b45
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-06 16:09:53 +0530

    start create CustomerService

--- src/main/java/lk/ijse/possystembackendwithspring/service/CustomerService.java
+++ src/main/java/lk/ijse/possystembackendwithspring/service/CustomerService.java
@@ -1,4 +1,12 @@
 package lk.ijse.possystembackendwithspring.service;
 
+import lk.ijse.possystembackendwithspring.dto.CustomerDTO;
+
+import java.util.ArrayList;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO dto);
+    void updateCustomer(CustomerDTO dto,String id);
+    void deleteCustomer(String id);
+    ArrayList<CustomerDTO> getAllCustomers();
 }
--- src/main/java/lk/ijse/possystembackendwithspring/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/possystembackendwithspring/service/impl/CustomerServiceImpl.java
@@ -2,13 +2,36 @@ package lk.ijse.possystembackendwithspring.service.impl;
 
 import jakarta.transaction.Transactional;
 import lk.ijse.possystembackendwithspring.dao.CustomerDAO;
+import lk.ijse.possystembackendwithspring.dto.CustomerDTO;
 import lk.ijse.possystembackendwithspring.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerDAO customerDAO;
+
+    @Override
+    public void saveCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO dto, String id) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers() {
+        return null;
+    }
 }
