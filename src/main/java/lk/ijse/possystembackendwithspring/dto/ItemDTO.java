d994328cd92692c03b74508b606ae2b53e3da770
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-06 12:19:16 +0530

    create iemDto

---
+++ src/main/java/lk/ijse/possystembackendwithspring/dto/ItemDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.possystembackendwithspring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO implements Serializable {
+    String item_code;
+    String item_Name;
+    String item_price;
+    String item_qty;
+}
