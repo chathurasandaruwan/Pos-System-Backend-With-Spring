b101bfbf1ab6fda0224c4f1cdaceecdae841f6f6
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-06 12:28:24 +0530

    create OrderEntity

---
+++ src/main/java/lk/ijse/possystembackendwithspring/entity/Order.java
@@ -0,0 +1,19 @@
+package lk.ijse.possystembackendwithspring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Order {
+    @Id
+    String order_id;
+    String item_code;
+    String order_date;
+    String customer_id;
+}
