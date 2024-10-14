abf7462ce8aa09b7471d424372a6313e9a141466
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-14 14:23:23 +0530

    add logs to ItemController class

--- src/main/java/lk/ijse/possystembackendwithspring/controller/ItemController.java
+++ src/main/java/lk/ijse/possystembackendwithspring/controller/ItemController.java
@@ -5,6 +5,8 @@ import lk.ijse.possystembackendwithspring.dto.ItemDTO;
 import lk.ijse.possystembackendwithspring.exeption.CustomerNotFoundException;
 import lk.ijse.possystembackendwithspring.exeption.DataPersistException;
 import lk.ijse.possystembackendwithspring.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/item")
 public class ItemController {
+    static Logger logger =  LoggerFactory.getLogger(ItemController.class);
     @Autowired
     private ItemService itemService;
 
@@ -23,10 +26,15 @@ public class ItemController {
     public ResponseEntity<Void> saveItem(@RequestBody ItemDTO itemDTO) {
         try {
             itemService.saveItem(itemDTO);
+            logger.info("From Item: "+"Item Saved Successfully !!!");
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            e.printStackTrace();
+            logger.warn("From Item: "+e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
+            e.printStackTrace();
+            logger.error("From Item: "+"Item Not Saved !!!");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -34,15 +42,19 @@ public class ItemController {
     public ResponseEntity<Void> updateItem(@PathVariable("itemCode") String itemCode,@RequestBody ItemDTO itemDTO) {
         try {
             if(itemDTO == null){
+                logger.warn("From Item: "+"ItemDTO Is Empty !!!");
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             itemService.updateItem(itemDTO,itemCode);
+            logger.info("From Item: "+"Item Updated Successfully !!!");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (CustomerNotFoundException e){
             e.printStackTrace();
+            logger.warn("From Item: "+e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             e.printStackTrace();
+            logger.error("From Item: "+"Item Not Updated !!!");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -51,16 +63,21 @@ public class ItemController {
     public ResponseEntity<Void> deleteItem(@PathVariable("itemCode") String itemCode) {
         try {
             itemService.deleteItem(itemCode);
+            logger.info("From Item: "+"Item Deleted Successfully !!!");
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (CustomerNotFoundException e) {
             e.printStackTrace();
+            logger.warn("From Item: "+e.getMessage());
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }catch (Exception e){
+            e.printStackTrace();
+            logger.error("From Item: "+"Item Not Deleted !!!");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ItemDTO> getAllItems() {
+        logger.info("From Item: "+"Item Get Successfully !!!");
         return itemService.getAllItems();
     }
 
