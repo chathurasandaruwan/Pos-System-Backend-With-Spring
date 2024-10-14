0079be356c8b8d2a02f9c6738f29f7aae8e8a3e6
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-14 14:29:08 +0530

    add logs to OrderController class

--- src/main/java/lk/ijse/possystembackendwithspring/controller/OrderController.java
+++ src/main/java/lk/ijse/possystembackendwithspring/controller/OrderController.java
@@ -5,6 +5,8 @@ import lk.ijse.possystembackendwithspring.dto.OrderDTO;
 import lk.ijse.possystembackendwithspring.entity.impl.Order;
 import lk.ijse.possystembackendwithspring.exeption.DataPersistException;
 import lk.ijse.possystembackendwithspring.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,25 +19,29 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/order")
 public class OrderController {
+    static Logger logger =  LoggerFactory.getLogger(OrderController.class);
     @Autowired
     private OrderService orderService;
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveOrder(@RequestBody OrderDTO orderDTO) {
         try {
             orderService.saveOrder(orderDTO);
+            logger.info("From Order: "+"Order Save Successfully !!!");
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            e.printStackTrace();
+            logger.warn("From Order: "+e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
+            e.printStackTrace();
+            logger.info("From Order: "+"Order Not Save !!!");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<OrderDTO> getAllOrders() {
         List<OrderDTO> allOrders = orderService.getAllOrders();
-        for (OrderDTO orderDTO : allOrders) {
-            System.out.println("From get order "+orderDTO.getOrder_id());
-        }
+        logger.info("From Order: "+"Order Get Successfully !!!");
         return allOrders;
     }
 }
